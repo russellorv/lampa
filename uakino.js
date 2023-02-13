68481382156aab1959e748c824995f33b5ebaf98
Ruslan O <russellorv@gmail.com>
2023-02-13 23:05:01 +0200

    step 1

--- uakino.js
+++ uakino.js
@@ -1394,7 +1394,8 @@
             network["native"](url, function (text) {
 
                 var str = text.replace(/\n/, '');
-                var links = str.match(/<a(.*?)movie-title(.*?)<\/a>/g);
+                str = str.replace(/\r\n/, '');
+                var links = str.match(/<a class="movie-title"[^>]+>(.*?)<\/a>/g);
 
                 var relise = object.search_date || (object.movie.number_of_seasons ? object.movie.first_air_date : object.movie.release_date) || '0000';
                 var need_year = parseInt((relise + '').slice(0, 4));
