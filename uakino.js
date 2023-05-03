2180a6e43573e6664709fd0f079777b393f089f9
Ruslan O <russellorv@gmail.com>
2023-05-03 22:00:14 +0300

    step 1

--- uakino.js
+++ uakino.js
@@ -51,8 +51,6 @@
                 var need_year = parseInt((relise + '').slice(0, 4));
                 var found_url = '';
 
-                console.log(links2);
-
                 if (links) {
                     var cards = [];
 
