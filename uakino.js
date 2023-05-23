5072b84bdc591d231a43c28819e72832da51b9ba
Ruslan O <russellorv@gmail.com>
2023-05-23 14:48:20 +0300

    step 1

--- uakino.js
+++ uakino.js
@@ -303,7 +303,7 @@
                     var find_video = str.match(/<link itemprop="video" value="(.*?)">/);
                     var find_video_title = str.match(/property="og:title" content="(.*?)"/);
 
-                    var find_h1_title = str.match(/<span class="solototle" itemprop="name">(.*?)<\/span>/);
+                    var find_h1_title = str.match(/<span\s+class="solototle"\s+itemprop="name">(.*?)<\/span>/);
 
                     console.log(find_h1_title);
                     console.log('h1');
